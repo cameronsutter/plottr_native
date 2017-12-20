fef2154cf9d6d58ee7d5abc1bf5deb98f052b2c1
Cameron Sutter <cameronsutter0@gmail.com>
2017-12-20 16:03:29 -0700

    edit line/scene in cards

--- app/components/cardDetails.js
+++ app/components/cardDetails.js
@@ -1,3 +1,4 @@
+import _ from 'lodash'
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
@@ -11,6 +12,8 @@ import {
   SectionList,
   TouchableOpacity,
   Button,
+  Picker,
+  LayoutAnimation,
 } from 'react-native'
 import Icon from 'react-native-vector-icons/FontAwesome'
 import AppStyles from '../styles'
@@ -18,43 +21,146 @@ import AppStyles from '../styles'
 class CardDetails extends Component {
   static navigationOptions = ({ navigation }) => {
     const { params } = navigation.state
-    let button = <Button
-      title='Save'
-      onPress={params.handleSave ? params.handleSave : () => null}
-    />
+    let right = null
+    if (params.dirty) {
+      right = <Button
+        title='Save'
+        onPress={params.handleSave ? params.handleSave : () => null}
+      />
+    } else {
+      right = <Text style={styles.green}>Saved</Text>
+    }
     return {
       title: `${params.card.title}`,
-      headerRight: button,
+      headerRight: right,
     }
   }
 
   constructor(props) {
     const { card } = props.navigation.state.params
     super(props)
+    this.renderedLines = this.renderLines()
+    this.renderedScenes = this.renderScenes()
     this.state = {
       title: card.title,
       description: card.description,
+      lineId: card.lineId,
+      sceneId: card.sceneId,
+      editLine: false,
+      editScene: false,
     }
   }
 
   handleSave = () => {
     const { card } = this.props.navigation.state.params
     this.props.actions.editCard(card.id, this.state.title, this.state.description)
+    this.props.navigation.setParams({dirty: false})
+    LayoutAnimation.easeInEaseOut()
+    this.setState({editLine: false, editScene: false})
   }
 
   componentWillMount () {
     this.props.navigation.setParams({handleSave: this.handleSave})
   }
 
+  titleChanged = (text) => {
+    this.props.navigation.setParams({dirty: true})
+    this.setState({title: text})
+  }
+
+  descriptionChanged = (text) => {
+    this.props.navigation.setParams({dirty: true})
+    this.setState({description: text})
+  }
+
   renderTitle = ({index, item}) => {
-    return <View style={styles.title}>
-      <TextInput onChangeText={(text) => this.setState({title: text})} multiline={true} defaultValue={item}/>
+    return <View style={styles.inputWrapper}>
+      <TextInput onChangeText={this.titleChanged} style={styles.input} multiline={true} defaultValue={item}/>
     </View>
   }
 
   renderDescription = ({index, item}) => {
-    return <View style={styles.content}>
-      <TextInput onChangeText={(text) => this.setState({description: text})} multiline={true} defaultValue={item}/>
+    return <View style={styles.inputWrapper}>
+      <TextInput onChangeText={this.descriptionChanged} style={styles.input} multiline={true} defaultValue={item}/>
+    </View>
+  }
+
+  renderLines = () => {
+    return this.props.lines.map((l) => <Picker.Item key={`l-${l.id}`} label={l.title} value={l.id}/> )
+  }
+
+  toggleEditLine = () => {
+    LayoutAnimation.easeInEaseOut()
+    let newValue = true
+    if (this.state.editLine) {
+      newValue = false
+    }
+    this.setState({editLine: newValue})
+  }
+
+  changeLine = (value) => {
+    this.setState({lineId: parseInt(value)})
+    this.props.navigation.setParams({dirty: true})
+  }
+
+  renderLine = ({index, item}) => {
+    let line = _.find(this.props.lines, {id: this.state.lineId})
+    let color = {color: line.color}
+    let picker = null
+    if (this.state.editLine) {
+      picker = <Picker
+        selectedValue={this.state.lineId}
+        onValueChange={this.changeLine}
+        >
+        { this.renderedLines }
+      </Picker>
+    }
+    return <View>
+      <TouchableOpacity onPress={this.toggleEditLine}>
+        <View style={styles.fakeInputWrapper}>
+          <Text style={[styles.input, color]}>{line.title}</Text>
+        </View>
+      </TouchableOpacity>
+      { picker }
+    </View>
+  }
+
+  renderScenes = () => {
+    return this.props.scenes.map((s) => <Picker.Item key={`s-${s.id}`} label={s.title} value={s.id}/> )
+  }
+
+  toggleEditScene = () => {
+    LayoutAnimation.easeInEaseOut()
+    let newValue = true
+    if (this.state.editScene) {
+      newValue = false
+    }
+    this.setState({editScene: newValue})
+  }
+
+  changeScene = (value) => {
+    this.setState({sceneId: parseInt(value)})
+    this.props.navigation.setParams({dirty: true})
+  }
+
+  renderScene = ({index, item}) => {
+    let line = _.find(this.props.scenes, {id: this.state.sceneId})
+    let picker = null
+    if (this.state.editScene) {
+      picker = <Picker
+        selectedValue={this.state.sceneId}
+        onValueChange={this.changeScene}
+        >
+        { this.renderedScenes }
+      </Picker>
+    }
+    return <View>
+      <TouchableOpacity onPress={this.toggleEditScene}>
+        <View style={styles.fakeInputWrapper}>
+          <Text style={styles.input}>{line.title}</Text>
+        </View>
+      </TouchableOpacity>
+      { picker }
     </View>
   }
 
@@ -64,6 +170,8 @@ class CardDetails extends Component {
       <SectionList
         sections={[
           {data: [card.title || ''], title: 'Title', renderItem: this.renderTitle},
+          {data: ['lineId'], title: 'Line', renderItem: this.renderLine},
+          {data: ['sceneId'], title: 'Scene', renderItem: this.renderScene},
           {data: [card.description || ''], title: 'Description', renderItem: this.renderDescription},
         ]}
         renderSectionHeader={({section}) => <View style={styles.sectionHeader}><Text style={styles.sectionHeaderText}>{section.title}</Text></View>}
@@ -77,11 +185,12 @@ const styles = StyleSheet.create({
   container: AppStyles.detailsView,
   sectionHeader: AppStyles.sectionHeader,
   sectionHeaderText: AppStyles.sectionHeaderText,
-  title: {
-    padding: 10,
-  },
-  content: {
-    padding: 10,
+  input: AppStyles.input,
+  green: AppStyles.greenSaved,
+  inputWrapper: AppStyles.inputWrapper,
+  fakeInputWrapper: {
+    ...AppStyles.inputWrapper,
+    paddingTop: 5,
   },
 })
 
@@ -94,11 +203,15 @@ CardDetails.propTypes = {
       })
     }).isRequired,
   }).isRequired,
+  lines: PropTypes.array.isRequired,
+  scenes: PropTypes.array.isRequired,
   actions: PropTypes.object.isRequired,
 }
 
 function mapStateToProps (state) {
   return {
+    lines: state.lines,
+    scenes: state.scenes,
   }
 }
 
