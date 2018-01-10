65839c1059ec0465729b93795f6182abf9cce2db
Cameron Sutter <cameronsutter0@gmail.com>
2018-01-10 14:11:51 -0700

    no saving to files
    Saving to files will not work that way. I need to rethink that strategy

--- app/middlewares/saver.js
+++ app/middlewares/saver.js
@@ -1,15 +1,16 @@
-import ActionTypes, { FILE_SAVED } from 'pltr'
+import { ActionTypes } from 'pltr'
 import { AsyncStorage } from 'react-native'
-// import RNFS from 'react-native-fs'
+
+const { FILE_SAVED, FILE_LOADED, NEW_FILE } = ActionTypes
+const BLACKLIST = [FILE_SAVED, FILE_LOADED]
 
 const saver = store => next => action => {
   const result = next(action)
-  if (action.type === FILE_SAVED) return result
+  if (BLACKLIST.includes(action.type)) return result
   // var isNewFile = action.type === NEW_FILE
   const state = store.getState()
   AsyncStorage.setItem('@Plottr:file', JSON.stringify(state))
-  // save back to the file
-  // RNFS.writeFile(state.file.fileName, state)
+  // TODO: save back to the file
   return result
 }
 
--- index.js
+++ index.js
@@ -8,8 +8,6 @@ import {
   Text,
   View,
   Image,
-  TabBarIOS,
-  NavigatorIOS,
   TouchableOpacity,
   NativeModules,
   AsyncStorage,
@@ -25,7 +23,7 @@ const store = configureStore({})
 
 export class App extends Component {
   constructor(props) {
-    super(props);
+    super(props)
 
     this.state = {
       plusIcon: null,
@@ -50,23 +48,18 @@ export class App extends Component {
     }
   }
 
-  fakeData () {
-    return FILE_DATA
-  }
-
   openFile = async () => {
     DocumentPicker.show({
       filetype: ['public.item'],
     }, async (error, fileInfo) => {
-      console.log(fileInfo)
       if (fileInfo.fileName.includes('.pltr')) {
         const response = await fetch(fileInfo.uri)
         const data = await response.json()
-        this.setState({ data, fileName: fileInfo.fileName })
-        store.dispatch(uiActions.loadFile(fileInfo.fileName, false, data, data.file.version))
+        this.setState({ data, fileName: fileInfo.uri })
+        store.dispatch(uiActions.loadFile(fileInfo.uri, false, data, data.file.version))
         try {
           await AsyncStorage.setItem('@Plottr:file', JSON.stringify(data))
-          await AsyncStorage.setItem('@Plottr:fileName', fileInfo.fileName)
+          await AsyncStorage.setItem('@Plottr:fileName', fileInfo.uri)
         } catch (error) {
           console.log(error)
         }
@@ -128,45 +121,4 @@ const styles = StyleSheet.create({
   },
 })
 
-AppRegistry.registerComponent('plottr_native', () => App);
-
-
-const FILE_DATA = {
-  "characters": [
-    {"id": 0, "name": "Leywa", "description": "description", "notes": "some notes here"},
-    {"id": 1, "name": "Tahu", "description": "description", "notes": "some notes here"}
-  ],
-  "places": [
-    {"id": 0, "name": "Holy Circle", "description": "a cool village", "notes": "some notes here"},
-    {"id": 1, "name": "Pepperoni Ridge", "description": "an even cooler village", "notes": "some notes here"}
-  ],
-  "tags": [
-    {"id": 0, "title": "Happy", "color": "orange"},
-    {"id": 1, "title": "Earthy", "color": "green"}
-  ],
-  "cards": [
-    {"id": 0, "title": "Beginning of the Story", "sceneId": 0, "lineId": 0, "description": "cool stuff"},
-    {"id": 1, "title": "Middle of the story", "sceneId": 1, "lineId": 0, "description": "more cool stuff"},
-    {"id": 2, "title": "Beginning of the subplot", "sceneId": 0, "lineId": 1, "description": "subplot stuff Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi quis ipsum felis. Maecenas vel leo magna. Maecenas nibh ligula, eleifend ut malesuada vitae, mollis non leo. Ut consequat neque nec magna rutrum mattis. Maecenas molestie congue bibendum. Integer et nunc vestibulum, maximus mauris congue, mattis elit. Aliquam eget libero tincidunt, lobortis elit eu, posuere enim. Etiam sed ultrices est, non dignissim nisl. Vivamus aliquet, risus ut sagittis accumsan, dolor elit pulvinar lorem, vitae laoreet diam mi ac tellus. Mauris id tincidunt libero. Vestibulum nec sodales ex, eget commodo neque.Etiam a dapibus arcu. Nullam malesuada erat eu leo viverra sagittis. Pellentesque laoreet dictum sagittis. Curabitur nec vestibulum quam. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vitae viverra nisi, quis suscipit tortor. In at odio vestibulum, imperdiet lorem in, eleifend tortor. Suspendisse mollis tristique libero, eu bibendum lacus sagittis in. Curabitur at scelerisque mi. Cras eget accumsan sapien, vitae cursus massa. Nam at lorem lacinia, ullamcorper dolor sit amet, fermentum ipsum. Ut a scelerisque sem, vel semper neque. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Fusce sit amet lectus ullamcorper ligula ornare vestibulum. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Mauris luctus lacus malesuada bibendum feugiat."}
-  ],
-  "scenes": [
-    {"title": "beginning", "id": 0, "position": 0},
-    {"title": "middle", "id": 1, "position": 1},
-    {"title": "end", "id": 2, "position": 2}
-  ],
-  "lines": [{"id": 0, "title": "Main Plot", "color": "red"}, {"id": 1, "title": "Sub Plot", "color": "blue"}],
-  "notes": [
-    {"title": "a note", "id": 0, "content": "here is some content"},
-    {"title": "another note", "id": 1, "content": "here is some content"}
-  ],
-  "customAttributes": {
-    "characters": ['age', 'power', 'gender'],
-    "places": ['weather', 'planet'],
-    "cards": [],
-    "scenes": [],
-    "lines": []
-  },
-  "file": {},
-  "storyName": "Pizza Planet 2",
-  "ui": {"currentView": "timeline", "orientation": "horizontal"}
-}
+AppRegistry.registerComponent('plottr_native', () => App)
